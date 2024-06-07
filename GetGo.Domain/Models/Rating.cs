70c76bdb321b7ae2763a45289c1fcc6e188f5095
hntduong12345 <hntduong2003@gmail.com>
2024-06-07 23:18:48 +0700

    fix data conflict bug between model and data

--- GetGo.Domain/Models/Rating.cs
+++ GetGo.Domain/Models/Rating.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace GetGo.Domain.Models
 {
     public class Rating
     {
+        [JsonPropertyName("1")]
         public float star1 { get; set; }
+        [JsonPropertyName("2")]
         public float star2 { get; set; }
+        [JsonPropertyName("3")]
         public float star3 { get; set; }
+        [JsonPropertyName("4")]
         public float star4 { get; set; }
+        [JsonPropertyName("5")]
         public float star5 { get; set; }
     }
 }
